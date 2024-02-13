6f5900f2d6c1ca071a09962954cb6b66d92b0bc4
Nidhi Satyapriya <satyapriyanidhi@gmail.com>
2024-02-13 23:02:45 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+ function App() {
+
+  return (
+   <div>
+    <Navbar title="" />
+
+   </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+//import styled from 'styled-components';
+im//port PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const home = () => {
+    return <div>
+        s
+    </div>;
+}
+
+home.propTypes = propTypes;
+home.defaultProps = defaultProps;
+// #endregion
+
+export default home;
